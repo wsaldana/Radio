9f1af54af58e0bc4ff46ebaa57ff3ca9a60c1589
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-01-18 13:49:48 -0600

    Declaración de metodos de interfaz

--- Funcionalidades.java
+++ Funcionalidades.java
@@ -5,5 +5,11 @@ Funcionalidades.java		Fecha de creación: 16/01/2020
 Interfaz que contiene las operaciones que puede realizar el Radio
 **********************************************************/
 public interface Funcionalidades{
-
+	public String estacionActual();
+	public boolean estado();
+	public void onOff();
+	public void cambiarFrecuencia();
+	public void avanzar();
+	public void guardar(int boton);
+	public void seleccionarEmisiora(int boton);
 }
